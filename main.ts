85d741b67cf244cd07e9c67e2eb3325a51e28b65
okdwiajci <103046074+okdwiajci@users.noreply.github.com>
2022-04-05 09:01:34 -0400

    none

--- main.ts
+++ main.ts
@@ -1,3 +1,57 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    password.push(char.removeAt(39))
 })
+input.onButtonPressed(Button.B, function () {
+    for (let value of password) {
+        basic.showString("" + (value))
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    password = []
+})
+let password: string[] = []
+let char: string[] = []
+char = [
+"a",
+"b",
+"c",
+"d",
+"e",
+"f",
+"g",
+"h",
+"i",
+"j",
+"k",
+"l",
+"m",
+"o",
+"p",
+"q",
+"r",
+"s",
+"t",
+"u",
+"v",
+"w",
+"x",
+"y",
+"z",
+"1",
+"2",
+"3",
+"4",
+"5",
+"6",
+"7",
+"8",
+"9",
+"0",
+"!",
+"@",
+"#",
+"$",
+"&"
+]
+password = []
